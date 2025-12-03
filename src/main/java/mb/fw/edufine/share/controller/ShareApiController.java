ce9e0e545313e2e604512ccd2674da5ac37effa1
250318 <250318@DESKTOP-7PODTUV>
2025-12-03 17:33:37 +0900

    인더 군자 투삼~  - 처리 내용 확인

--- src/main/java/mb/fw/edufine/share/controller/ShareApiController.java
+++ src/main/java/mb/fw/edufine/share/controller/ShareApiController.java
@@ -1,12 +1,17 @@
 package mb.fw.edufine.share.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import mb.fw.edufine.share.constants.ShareServiceCode;
 import mb.fw.edufine.share.dto.EsbRequestMessage;
@@ -43,4 +48,64 @@ public class ShareApiController {
 		return null;
 
 	}
+
+	public static void main(String[] args) {
+		Map<String, Object> root = new HashMap<>();
+		List<Map<String, Object>> dataList = new ArrayList<>();
+		Map<String, Object> paramMap = new HashMap<>();
+		paramMap.put("CVA_KND_CD", "B0007");
+		paramMap.put("TXPR_CL_CD", "01");
+		paramMap.put("TXPR_NM", "홍길동");
+		paramMap.put("RESNO", "555");
+		paramMap.put("BSNO", "333");
+		paramMap.put("CPRNO", "123");
+		paramMap.put("TELNO", "");
+		paramMap.put("MPNO", "");
+		paramMap.put("EML", "");
+		paramMap.put("ROAD_NM_ADR", "");
+		paramMap.put("LD_ADR", "");
+		paramMap.put("RESNO_OP_YN", "");
+		paramMap.put("ADR_OP_YN", "");
+		paramMap.put("CERP_ISN_RQS_QTY", "");
+		paramMap.put("CVA_DCUM_GRAN_MTHD_CD", "");
+		paramMap.put("CVA_DCUM_SBMS_ORGN_CL_CD", "");
+		paramMap.put("PLSB_NM", "");
+		paramMap.put("NNF_CL_CD", "");
+		paramMap.put("PSP_RGT_NO", "");
+		paramMap.put("PSP_FNM", "홍길동");
+		paramMap.put("PSP_RESNO", "11111111111111");
+		paramMap.put("PSP_CRTF", "2222222222");
+		paramMap.put("PRIC_PYMN_AMT", "1023");
+		paramMap.put("PRIC_APE_DT", "333");
+		paramMap.put("PRIC_PYMN_CHRG_FNM", "");
+		paramMap.put("PRIC_PYMN_TEL_NO", "000-111-2222");
+		paramMap.put("RCAT_MTHD_CD", "23");
+		paramMap.put("ISN_APLN_ORGN_CD", "1741000");
+		paramMap.put("ISN_APLN_ORGN_TELNO", "");
+		dataList.add(paramMap);
+		root.put("data", dataList);
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String bodyStr = gson.toJson(root);
+
+		System.out.println(bodyStr);
+		// 1. bodyStr 암호화, 전자서명, 인코딩 처리
+		
+		// 2. http 헤더 구성(api key 추가)
+		
+		// 3. 행공 call
+		
+		// 4. 행공 응답 str 디코딩, 서명체크, 복호화
+		
+		// 5. str -> object
+		Map<String, Object> bodyMap = gson.fromJson(bodyStr, Map.class);
+		ArrayList<Map<String, Object>> resultList = (ArrayList) bodyMap.get("data");
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("esb_data", resultList);
+		System.out.println(gson.toJson(resultMap));
+		
+		// 6. esb_data 에 담아서 응답
+		
+		
+	}
+
 }
